1450af1a918a67dea9259ddead30519d405bf604
Eva Maxfield Brown <evamaxfield@github.com>
2025-10-22 19:14:33 -0700

    Lint and format

--- repo_statistics/gini.py
+++ repo_statistics/gini.py
@@ -4,10 +4,11 @@ import numpy as np
 
 ###############################################################################
 
+
 def _compute_gini(arr: list[int]) -> float:
     # From: https://stackoverflow.com/a/61154922
-    arr = np.array(arr)
+    np_arr = np.array(arr)
     diffsum = 0
-    for i, xi in enumerate(arr[:-1], 1):
-        diffsum += np.sum(np.abs(xi - arr[i:]))
-    return diffsum / (len(arr)**2 * np.mean(arr))
+    for i, xi in enumerate(np_arr[:-1], 1):
+        diffsum += np.sum(np.abs(xi - np_arr[i:]))
+    return diffsum / (len(np_arr) ** 2 * np.mean(np_arr))
